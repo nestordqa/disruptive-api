e6f8c93d1c5f280687553b93d8f0d0db99fa65ba
Nestor Quinones <ndqa96@gmail.com>
2024-10-27 16:41:53 -0400

    fix: fixing categories bug

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -3,10 +3,10 @@ import Category from '../models/Category';
 
 //Crear nueva categoria
 export const createCategory = async (req: Request, res: Response): Promise<void> => {
-  const { name } = req.body;
+  const { name, permissions } = req.body;
 
   try {
-    const category = new Category({ name });
+    const category = new Category({ name, permissions });
     await category.save();
     res.status(201).json(category);
   } catch (error: any) {
@@ -42,10 +42,10 @@ export const getCategoryById = async (req: Request, res: Response): Promise<any>
 //Actualiza categoria por el ID
 export const updateCategory = async (req: Request, res: Response): Promise<any> => {
   const { id } = req.params;
-  const { name } = req.body;
+  const { name, permissions } = req.body;
 
   try {
-    const updatedCategory = await Category.findByIdAndUpdate(id, { name }, { new: true });
+    const updatedCategory = await Category.findByIdAndUpdate(id, { name, permissions }, { new: true });
     if (!updatedCategory) {
       return res.status(404).json({ error: 'Categoría no encontrada' });
     }
